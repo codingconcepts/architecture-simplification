4c56149c3dbdbd6d4febbfd3982e23a479ea5b0a
codingconcepts <hello@robreid.io>
2023-12-05 14:58:18 +0000

    feat: Working before and after as per Ben's Square experience

--- 001_fragile_data_integrations/tightly_coupled_services/before/services/stock/main.go
+++ 001_fragile_data_integrations/tightly_coupled_services/before/services/stock/main.go
@@ -2,30 +2,49 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/segmentio/kafka-go"
 )
 
 func main() {
 	time.Sleep(time.Second * 20)
 
-	db, err := pgxpool.New(context.Background(), "postgres://stock@cockroachdb:26257/stock?sslmode=disable")
+	stockDB, err := pgxpool.New(context.Background(), "postgres://stock@stock_db:26257/stock?sslmode=disable")
 	if err != nil {
 		log.Fatalf("error connecting to database: %v", err)
 	}
-	defer db.Close()
+	defer stockDB.Close()
 
-	if err = db.Ping(context.Background()); err != nil {
-		log.Fatalf("error pinging database: %v", err)
+	productDB, err := pgxpool.New(context.Background(), "postgres://stock@stock_db:26257/product?sslmode=disable")
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
 	}
+	defer productDB.Close()
+
+	rawReader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     []string{"redpanda:29092"},
+		GroupID:     uuid.NewString(),
+		Topic:       "products",
+		StartOffset: kafka.LastOffset,
+	})
+
+	go updateProducts(productDB, rawReader)
 
 	router := fiber.New()
-	router.Get("/stock/:id", handleGetStock(db))
+	router.Get("/stock/:id", handleGetStock(stockDB, productDB))
 
 	log.Fatal(router.Listen(":3000"))
+}
+
+type Product struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 type Stock struct {
@@ -33,27 +52,64 @@ type Stock struct {
 	QuantityOnHand int    `json:"quantity_on_hand"`
 }
 
-func handleGetStock(db *pgxpool.Pool) fiber.Handler {
+func handleGetStock(stock, product *pgxpool.Pool) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		productID := c.Params("id")
 		if productID == "" {
 			return fiber.NewError(fiber.StatusUnprocessableEntity, "missing product id")
 		}
 
-		const stmt = `SELECT quantity_on_hand FROM stock WHERE product_id = $1`
+		// Fetch stock data.
+		const stockStmt = `SELECT quantity_on_hand FROM stock WHERE product_id = $1`
 
-		row := db.QueryRow(c.Context(), stmt, productID)
+		row := stock.QueryRow(c.Context(), stockStmt, productID)
 
-		p := Stock{
+		s := Stock{
 			ProductID: productID,
 		}
-		if err := row.Scan(&p.QuantityOnHand); err != nil {
+		if err := row.Scan(&s.QuantityOnHand); err != nil {
+			return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+		}
+
+		// Fetch product data.
+		const productStmt = `SELECT name FROM products WHERE id = $1`
+
+		row = product.QueryRow(c.Context(), productStmt, productID)
+
+		var p Product
+		if err := row.Scan(&p.Name); err != nil {
 			return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 		}
 
 		return c.JSON(fiber.Map{
-			"id":       p.ProductID,
-			"quantity": p.QuantityOnHand,
+			"id":       productID,
+			"name":     p.Name,
+			"quantity": s.QuantityOnHand,
 		})
+	}
+}
+
+func updateProducts(db *pgxpool.Pool, reader *kafka.Reader) {
+	for {
+		// Wait for message and parse it once received.
+		msg, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			log.Printf("error reading product message: %v", err)
+			continue
+		}
+
+		var p Product
+		if err = json.Unmarshal(msg.Value, &p); err != nil {
+			log.Printf("error parsing product message: %v", err)
+			continue
+		}
+
+		// Write the update to the local database.
+		const insertStmt = `UPSERT INTO products (id, name) VALUES ($1, $2)`
+
+		if _, err = db.Exec(context.Background(), insertStmt, p.ID, p.Name); err != nil {
+			log.Printf("error upserting product: %v", err)
+			continue
+		}
 	}
 }
--- 001_fragile_data_integrations/tightly_coupled_services/steps.md
+++ 001_fragile_data_integrations/tightly_coupled_services/steps.md
@@ -2,6 +2,8 @@
 
 ### Infra
 
+Services
+
 ``` sh
 cp go.* 001_fragile_data_integrations/tightly_coupled_services/before/services/product
 cp go.* 001_fragile_data_integrations/tightly_coupled_services/before/services/stock
@@ -12,6 +14,41 @@ cp go.* 001_fragile_data_integrations/tightly_coupled_services/before/services/s
 )
 ```
 
+Generate license
+
+``` sh
+cockroach sql \
+  --url "postgresql://root@localhost:26001/?sslmode=disable" \
+  -e "SELECT crdb_internal.cluster_id()"
+
+crl-lic -type "Evaluation" -org "Rob Test" -months 1 cdd0461c-219b-4642-b90f-9a8bb5dffc06
+```
+
+Connect to cluster
+
+``` sh
+cockroach sql --host localhost:26001 --insecure
+```
+
+Apply license
+
+``` sql
+SET CLUSTER SETTING cluster.organization = 'Rob Test';
+SET CLUSTER SETTING enterprise.license = 'crl-0-ChDN0EYcIZtGQrkPmou13/wGEI+x4KwGGAIiCFJvYiBUZXN0';
+```
+
+Create product changefeed
+
+``` sql
+SET CLUSTER SETTING kv.rangefeed.enabled = true;
+
+CREATE CHANGEFEED INTO 'kafka://redpanda:29092?topic_name=products'
+AS SELECT
+  "id",
+  "name"
+FROM product.products;
+```
+
 ### Test infra
 
 **Wait for 20s**
@@ -19,8 +56,14 @@ cp go.* 001_fragile_data_integrations/tightly_coupled_services/before/services/s
 ``` sh
 curl -s "http://localhost:3001/products/ac9384f7-12f7-4431-8a78-c9ccc6d321af" | jq
 curl -s "http://localhost:3002/stock/ac9384f7-12f7-4431-8a78-c9ccc6d321af" | jq
+```
 
-curl -s "http://localhost:3000/products/ac9384f7-12f7-4431-8a78-c9ccc6d321af" | jq
+Update source products and see stock products updated.
+
+``` sql
+UPDATE product.products
+SET "name" = 'new a'
+WHERE id = 'ac9384f7-12f7-4431-8a78-c9ccc6d321af';
 ```
 
 # After
