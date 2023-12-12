d1db031a009b70438f7cea01a43e575f0133ea6e
codingconcepts <hello@robreid.io>
2023-12-12 09:40:46 +0000

    feat: Working with Cockroach, still pre-bigquery

--- 005_unnecessary_dw_workloads/triplicating_data/before/s3-to-bigquery/main.go
+++ 005_unnecessary_dw_workloads/triplicating_data/before/s3-to-bigquery/main.go
@@ -2,20 +2,86 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"time"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func HandleRequest(ctx context.Context, s3Event events.S3Event) {
-	fmt.Println("HELLOOOOOO!")
-	for _, record := range s3Event.Records {
-		s3 := record.S3
-		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+func main() {
+	lambda.Start(handle)
+}
+
+func handle(ctx context.Context, e event) {
+	for _, record := range e.Records {
+		log.Printf("message id: %s", record.MessageID)
+
+		var b body
+		if err := json.Unmarshal([]byte(record.Body), &b); err != nil {
+			log.Fatalf("error unmarshalling body: %v", err)
+		}
+
+		for _, s3 := range b.Records {
+			fmt.Printf("s3:     %+v\n", s3.S3.Object.Key)
+		}
 	}
 }
 
-func main() {
-	lambda.Start(HandleRequest)
+type event struct {
+	Records []struct {
+		Attributes struct {
+			ApproximateFirstReceiveTimestamp string `json:"ApproximateFirstReceiveTimestamp"`
+			ApproximateReceiveCount          string `json:"ApproximateReceiveCount"`
+			SenderID                         string `json:"SenderId"`
+			SentTimestamp                    string `json:"SentTimestamp"`
+		} `json:"attributes"`
+		AwsRegion         string `json:"awsRegion"`
+		Body              string `json:"body"`
+		EventSource       string `json:"eventSource"`
+		EventSourceARN    string `json:"eventSourceARN"`
+		Md5OfBody         string `json:"md5OfBody"`
+		MessageAttributes struct {
+		} `json:"messageAttributes"`
+		MessageID     string `json:"messageId"`
+		ReceiptHandle string `json:"receiptHandle"`
+	} `json:"Records"`
+}
+
+type body struct {
+	Records []struct {
+		EventVersion string    `json:"eventVersion"`
+		EventSource  string    `json:"eventSource"`
+		AwsRegion    string    `json:"awsRegion"`
+		EventTime    time.Time `json:"eventTime"`
+		EventName    string    `json:"eventName"`
+		UserIdentity struct {
+			PrincipalID string `json:"principalId"`
+		} `json:"userIdentity"`
+		RequestParameters struct {
+			SourceIPAddress string `json:"sourceIPAddress"`
+		} `json:"requestParameters"`
+		ResponseElements struct {
+			XAmzRequestID string `json:"x-amz-request-id"`
+			XAmzID2       string `json:"x-amz-id-2"`
+		} `json:"responseElements"`
+		S3 struct {
+			S3SchemaVersion string `json:"s3SchemaVersion"`
+			ConfigurationID string `json:"configurationId"`
+			Bucket          struct {
+				Name          string `json:"name"`
+				OwnerIdentity struct {
+					PrincipalID string `json:"principalId"`
+				} `json:"ownerIdentity"`
+				Arn string `json:"arn"`
+			} `json:"bucket"`
+			Object struct {
+				Key       string `json:"key"`
+				Sequencer string `json:"sequencer"`
+				Size      int    `json:"size"`
+				ETag      string `json:"eTag"`
+			} `json:"object"`
+		} `json:"s3"`
+	} `json:"Records"`
 }
--- 005_unnecessary_dw_workloads/triplicating_data/steps.md
+++ 005_unnecessary_dw_workloads/triplicating_data/steps.md
@@ -9,20 +9,68 @@
 Compose
 
 ``` sh
-cp go.* 005_unnecessary_dw_workloads/triplicating_data/before/s3-to-bigquery
-
 (
   cd 005_unnecessary_dw_workloads/triplicating_data/before && \
   docker compose up --build --force-recreate -d
 )
 ```
 
+### CockroachDB
+
+Connect
+
+``` sh
+cockroach sql --insecure
+```
+
+Get cluster id
+
+``` sql
+SELECT crdb_internal.cluster_id();
+```
+
+Generate license
+
+``` sh
+crl-lic -type "Evaluation" -org "Rob Test" -months 1 265b8f5b-74fc-4dcf-b4b7-c321872bb870
+```
+
+Apply license
+
+``` sql
+SET CLUSTER SETTING cluster.organization = 'Rob Test';
+SET CLUSTER SETTING enterprise.license = 'crl-0-ChAmW49bdPxNz7S3wyGHK7hwEOWIhK0GGAIiCFJvYiBUZXN0';
+```
+
+Create table and changefeed
+
+``` sql
+CREATE TABLE orders (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id UUID NOT NULL,
+  total DECIMAL NOT NULL,
+  ts TIMESTAMP NOT NULL DEFAULT now()
+);
+
+SET CLUSTER SETTING kv.rangefeed.enabled = true;
+
+CREATE CHANGEFEED FOR TABLE orders
+  INTO 's3://s3-to-bigquery?AWS_ENDPOINT=http%3A%2F%2Fhost.docker.internal%3A4566&AWS_ACCESS_KEY_ID=fake&AWS_SECRET_ACCESS_KEY=fake&AWS_REGION=us-east-1';
+
+INSERT INTO orders (user_id, total) VALUES ('7559c90e-4990-4676-bbad-b8a65e8bc01d', 10.99);
+INSERT INTO orders (user_id, total) VALUES ('ca6c14bb-a49b-49e0-ad3c-d754eec17f22', 21.98);
+INSERT INTO orders (user_id, total) VALUES ('bd3b5245-664a-463f-967e-140d4a229691', 7.37);
+```
+
+### Localstack
+
 Terraform
 
 ``` sh
 (cd 005_unnecessary_dw_workloads/triplicating_data/before && terraform init)
+
+cp go.* 005_unnecessary_dw_workloads/triplicating_data/before/s3-to-bigquery
 (cd 005_unnecessary_dw_workloads/triplicating_data/before && terraform apply --auto-approve)
-(cd 005_unnecessary_dw_workloads/triplicating_data/before && terraform destroy --auto-approve)
 ```
 
 ### Test
@@ -38,12 +86,16 @@ awslocal sqs receive-message --queue-url http://sqs.us-east-1.localhost.localsta
 
 awslocal lambda get-function --function-name s3-to-bigquery
 
-
 awslocal --endpoint-url=http://localhost:4566 logs tail /aws/lambda/s3-to-bigquery --follow
 ```
 
 ### Teardown
 
 ``` sh
+# Just localstack.
 awslocal s3 rm s3://s3-to-bigquery --recursive
+(cd 005_unnecessary_dw_workloads/triplicating_data/before && terraform destroy --auto-approve)
+
+# Everything.
+make teardown
 ```
