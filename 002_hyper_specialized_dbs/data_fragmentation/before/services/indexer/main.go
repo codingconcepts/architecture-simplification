c9b134e8ee172641f24edb659a214bce3e48c97c
codingconcepts <hello@robreid.io>
2023-12-14 09:19:58 +0000

    feat: Working before scenario

--- 002_hyper_specialized_dbs/data_fragmentation/before/services/indexer/main.go
+++ 002_hyper_specialized_dbs/data_fragmentation/before/services/indexer/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/redis/go-redis/v9"
@@ -11,22 +15,32 @@ import (
 func main() {
 	log.SetFlags(0)
 
-	//time.Sleep(time.Second * 20)
+	redisURL, ok := os.LookupEnv("REDIS_URL")
+	if !ok {
+		log.Fatalf("missing REDIS_URL env var")
+	}
 
-	// // Connect to Postgres.
-	// db, err := pgxpool.New(context.Background(), "postgres://postgres:password@index:5432/postgres?sslmode=disable")
-	// if err != nil {
-	// 	log.Fatalf("error connecting to database: %v", err)
-	// }
-	// defer db.Close()
+	indexURL, ok := os.LookupEnv("INDEX_URL")
+	if !ok {
+		log.Fatalf("missing INDEX_URL env var")
+	}
 
-	// if err = db.Ping(context.Background()); err != nil {
-	// 	log.Fatalf("error pinging database: %v", err)
-	// }
+	time.Sleep(time.Second * 10)
+
+	// Connect to database.
+	db, err := pgxpool.New(context.Background(), indexURL)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(context.Background()); err != nil {
+		log.Fatalf("error pinging database: %v", err)
+	}
 
 	// Connect to Redis.
 	cache := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     redisURL,
 		Password: "",
 		DB:       0,
 	})
@@ -36,7 +50,26 @@ func main() {
 		log.Fatalf("error pinging redis: %v", err)
 	}
 
-	pipeToIndex(cache, nil)
+	mustEnsureIndex(db)
+	pipeToIndex(cache, db)
+}
+
+func mustEnsureIndex(db *pgxpool.Pool) {
+	const stmt = `CREATE TABLE IF NOT EXISTS "product" (
+									"id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+									"name" VARCHAR(255) NOT NULL,
+									"description" VARCHAR(255)
+								)`
+
+	if _, err := db.Exec(context.Background(), stmt); err != nil {
+		log.Fatalf("error creating product table: %v", err)
+	}
+}
+
+type product struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 func pipeToIndex(cache *redis.Client, db *pgxpool.Pool) {
@@ -51,6 +84,25 @@ func pipeToIndex(cache *redis.Client, db *pgxpool.Pool) {
 			continue
 		}
 
-		log.Printf("value: %s", string(raw))
+		var p product
+		if err = json.Unmarshal(raw, &p); err != nil {
+			log.Printf("error parsing update: %v", err)
+			continue
+		}
+
+		if err = writeToIndex(db, p); err != nil {
+			log.Printf("error writing update: %v", err)
+			continue
+		}
 	}
+}
+
+func writeToIndex(db *pgxpool.Pool, p product) error {
+	const stmt = `UPSERT INTO product (id, name, description) VALUES ($1, $2, $3)`
+
+	if _, err := db.Exec(context.Background(), stmt, p.ID, p.Name, p.Description); err != nil {
+		return fmt.Errorf("inserting product into index: %w", err)
+	}
+
+	return nil
 }
--- 002_hyper_specialized_dbs/data_fragmentation/steps.md
+++ 002_hyper_specialized_dbs/data_fragmentation/steps.md
@@ -13,20 +13,6 @@ cp go.* 002_hyper_specialized_dbs/data_fragmentation/before/services/indexer
 
 ### Run
 
-Connect to database
-
-``` sh
-psql "postgres://postgres:password@localhost:5432/postgres"
-```
-
-``` sql
-CREATE TABLE "product" (
-  "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  "name" VARCHAR(255) NOT NULL,
-  "description" VARCHAR(255)
-);
-```
-
 Connect to redis
 
 ``` sh
@@ -36,10 +22,36 @@ docker exec -it redis redis-cli
 Enable keyspace notifications
 
 ``` sh
-config set notify-keyspace-events s
+config set notify-keyspace-events KEA
 ```
 
+Write data
+
+``` sh
+SET e4619022-7f6f-4292-a158-673c25d7ed37 '{"id": "e4619022-7f6f-4292-a158-673c25d7ed37", "name": "Latte", "description": "A milky coffee"}'
+
+SET e8368c71-4786-484e-9101-da4396c5a411 '{"id": "e8368c71-4786-484e-9101-da4396c5a411", "name": "Cortado", "description": "A less milky coffee"}'
+```
+
+Connect to database
+
+``` sh
+cockroach sql --insecure
+```
+
+Check for updates
+
+``` sql
+SELECT * FROM product;
+```
+
+### Summary
+
+* Redis is a very capable data store, I just needed to demonstrate _some_ ingestion database.
+
 # After
+
+* Just the database
 
 ### Infra
 
